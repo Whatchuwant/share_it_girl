1d9cd5de0576f2480477bebe09a0a84f8be98ccd
Julie Reinken <jul.moon@gmail.com>
2015-12-08 14:00:12 -0500

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'http://rubygems.org'
+ruby '2.2.3'
+
+gem 'sinatra'
+gem 'sinatra-activerecord'
+gem 'activerecord'
+gem 'sqlite3'
+gem 'rake'
+gem 'rack-flash3'
---
+++ main.rb
@@ -0,0 +1,67 @@
+require 'sinatra'
+require 'sinatra/activerecord'
+require 'bundler/setup'
+require 'rack-flash'
+require './models'
+enable :sessions
+
+
+set :database, "sqlite3:funtimes.sqlite3"
+set :sessions, true
+use Rack::Flash, sweep: true
+
+############# Sign-up and thank you ####################
+get '/' do
+   @users = User.all 
+   erb :home
+end
+
+get '/signup' do
+	erb :signup
+end
+
+post '/signup' do
+	@user = User.create(username: params[:username], email: params[:email], password: params[:password])
+	session[:user_id] = @user.id
+	redirect to '/thanks'
+	puts "thanks for joining!"
+end
+
+get '/thanks' do
+	erb :thanks
+end
+
+########### http POST method and '/login' actionroute ###########
+get '/login' do
+	erb :login
+end
+
+post '/login' do
+	@user = User.where(email: params[:email]).first
+
+	if @user && @user.password == params[:password]
+		session[:user_id] = @user.id
+		flash[:info] = "You're now signed in"
+		redirect "/"
+
+	else
+		flash[:alert] = "Your password or email is incorrect"
+		redirect "/login"
+	end
+end
+
+get '/profile' do
+	erb :profile
+end
+
+post '/posts' do
+	@post = Post.create(body: params[:body])
+	# @posts = Post.where(user_id: session[:user_id])
+	erb :profile
+end
+
+get '/posts' do
+	@posts = Post.where(user_id: session[:user_id])
+	erb :posts
+end
+
