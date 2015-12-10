e492c93d9b6b1b0f31edd58243adc10b0c33cb0f
Julie Reinken <jul.moon@gmail.com>
2015-12-10 12:17:15 -0500

    added bcrypt

--- models.rb
+++ models.rb
@@ -1,7 +1,29 @@
 class User < ActiveRecord::Base
 	has_one :profile
-	has_secure_password
+	
+	  # users.password_hash in the database is a :string
+  
+  attr_accessor :password
+  validates_confirmation_of :password
+  before_save :encrypt_password
+
+  def encrypt_password
+  	self.password_salt = BCrypt::Engine.generate_salt
+	self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+  end
+ 
+  def self.authenticate(email, password)
+	user = User.where(email: email).first
+	if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+	user
+	else
+	nil
+    end
+  end
+  
 end
+
+
 
 class Post < ActiveRecord::Base
 # reference to table must be singular, even if table name is plural
